15d0d0033a22cbb31c178de3b23a0c52106a9bd5
atsushimemet <noap3b69n@gmail.com>
2024-01-23 20:28:43 +0900

    refactor: DataFrame_makerの移動

--- rakuten_api_call_bulk_from_table.py
+++ rakuten_api_call_bulk_from_table.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 import base64
-import csv
-import datetime
 import hashlib
-import json
-import os
 import re
-import sys
 from io import StringIO
 from time import sleep
 
@@ -15,28 +10,12 @@ import boto3
 import numpy as np
 import pandas as pd
 import pymysql
-import requests
 from sshtunnel import SSHTunnelForwarder
+
 from vook_db_v7.config import MAX_PAGE, REQ_URL, WANT_ITEMS, req_params
-from vook_db_v7.local_config import (
-    get_ec2_config,
-    get_rds_config,  # noqa
-    put_ec2_config,
-)
-
-
-class DualLogger:
-    def __init__(self, file_path, terminal):
-        self.terminal = terminal
-        self.log = open(file_path, "w")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        # このメソッドはダミーで、必要ない場合があります。
-        pass
+from vook_db_v7.local_config import get_rds_config  # noqa
+from vook_db_v7.local_config import get_ec2_config, put_ec2_config
+from vook_db_v7.utils import DataFrame_maker
 
 
 def main(event, context):
@@ -83,8 +62,6 @@ def main(event, context):
             # SQLクエリの実行
             cursor.execute(query)
             for row in cursor:  # column1, column2, ...は取得したいカラム名に合わせて変更してください
-                # print(row)
-
                 df_from_db = pd.concat(
                     [df_from_db, pd.DataFrame([row])], ignore_index=True
                 )
@@ -94,64 +71,6 @@ def main(event, context):
         finally:
             if conn is not None:
                 conn.close()
-
-    # apiコールした結果からdataframeを出力する関数を定義
-    def DataFrame_maker(keyword, platform_id, knowledge_id, size_id):
-        cnt = 1
-        df = pd.DataFrame(columns=WANT_ITEMS)
-        req_params["page"] = cnt
-        req_params["keyword"] = keyword
-        while True:
-            req_params["page"] = cnt
-            res = requests.get(REQ_URL, req_params)
-            res_code = res.status_code
-            res = json.loads(res.text)
-            if res_code != 200:
-                print(
-                    f"""
-                ErrorCode -> {res_code}\n
-                Error -> {res['error']}\n
-                Page -> {cnt}"""
-                )
-            else:
-                if res["hits"] == 0:
-                    print("返ってきた商品数の数が0なので、ループ終了")
-                    break
-                tmp_df = pd.DataFrame(res["Items"])[WANT_ITEMS]
-                df = pd.concat([df, tmp_df], ignore_index=True)
-            if cnt == MAX_PAGE:
-                print("MAX PAGEに到達したので、ループ終了")
-                break
-            # logger.info(f"{cnt} end!")
-            cnt += 1
-            # リクエスト制限回避
-            sleep(1)
-
-            print("Finished!!")
-        df["platform_id"] = platform_id
-        df["knowledge_id"] = knowledge_id
-        df["size_id"] = size_id
-        df_main = df.rename(
-            columns={"itemName": "name", "itemPrice": "price", "itemUrl": "url"}  # noqa
-        )
-        df_main = df_main.reindex(
-            columns=[
-                "id",
-                "name",
-                "url",
-                "price",
-                "knowledge_id",
-                "platform_id",
-                "size_id",
-            ]
-        )
-        print("price type before:", df_main["price"].dtype)
-        df_main["price"] = df_main["price"].astype(int)
-        print("price type after:", df_main["price"].dtype)
-        run_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-        df_main["created_at"] = run_time
-        df_main["updated_at"] = run_time
-        return df_main
 
     # 対応表を読み出し
     errata_table = pd.read_csv("./data/input/query_ng_ok.csv")
@@ -351,8 +270,6 @@ def main(event, context):
     created_at_checker(df_bulk)
     print(df_bulk.dtypes)
 
-    file_name = "products_raw_test"
-    # df_bulk.to_csv("./data/output/" + file_name + ".csv", index=False)
     print("finish")
 
     """作成したdataframをcsvとして　s３にも保存しておく"""
@@ -415,7 +332,8 @@ def main(event, context):
 
         try:
             conn = pymysql.connect(
-                **get_rds_config_for_put(server.local_bind_port), connect_timeout=10
+                **get_rds_config_for_put(server.local_bind_port),
+                connect_timeout=10,  # noqa
             )
             cursor = conn.cursor()
             # SQLクエリの実行
@@ -435,11 +353,11 @@ def main(event, context):
                     )
                 """
             # 既存DBの中身を削除する処理を記載
-            cursor.execute(f"TRUNCATE TABLE products")
+            cursor.execute("TRUNCATE TABLE products")
 
             cursor.execute(create_table_query)
             # DataFrameをRDSのテーブルに挿入
-            insert_query = "INSERT INTO products (id,name,url,price,knowledge_id,platform_id,size_id,created_at,updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            insert_query = "INSERT INTO products (id,name,url,price,knowledge_id,platform_id,size_id,created_at,updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"  # noqa
 
             for index, row in df_bulk.iterrows():
                 print(row)
@@ -472,7 +390,7 @@ def main(event, context):
     SELECT
         *
     FROM
-        products 
+        products
     """
     df_from_db = pd.DataFrame()
 
@@ -491,15 +409,14 @@ def main(event, context):
 
         try:
             conn = pymysql.connect(
-                **get_rds_config_for_put(server.local_bind_port), connect_timeout=10
+                **get_rds_config_for_put(server.local_bind_port),
+                connect_timeout=10,  # noqa
             )
 
             cursor = conn.cursor()
             # SQLクエリの実行
             cursor.execute(query)
             for row in cursor:  # column1, column2, ...は取得したいカラム名に合わせて変更してください
-                # print(row)
-
                 df_from_db = pd.concat(
                     [df_from_db, pd.DataFrame([row])], ignore_index=True
                 )
@@ -511,27 +428,3 @@ def main(event, context):
                 conn.close()
 
     print(df_from_db.head(), df_from_db.shape)
-
-
-# if __name__ == "__main__":
-#     now = datetime.datetime.now()
-#     date_str = now.strftime("%Y%m%d")
-#     time_str = now.strftime("%H%M%S")
-
-#     # ログディレクトリと日付サブディレクトリのパスを作成
-#     log_directory = os.path.join("log", date_str)
-
-#     # ディレクトリが存在しない場合は作成
-#     if not os.path.exists(log_directory):
-#         os.makedirs(log_directory)
-#     # ログファイルのパスを作成
-#     log_file_path = os.path.join(log_directory, f"log_{date_str}{time_str}.txt")  # noqa
-
-#     # DualLogger を設定
-#     sys.stdout = DualLogger(log_file_path, sys.stdout)
-
-#     # main 関数を実行
-#     main()
-
-#     # ログファイルを閉じる
-#     sys.stdout.log.close()
---
+++ vook_db_v7/utils.py
@@ -0,0 +1,63 @@
+import datetime
+import json
+
+import requests
+
+
+def DataFrame_maker(keyword, platform_id, knowledge_id, size_id):
+    """apiコールした結果からdataframeを出力する関数を定義"""
+    cnt = 1
+    df = pd.DataFrame(columns=WANT_ITEMS)
+    req_params["page"] = cnt
+    req_params["keyword"] = keyword
+    while True:
+        req_params["page"] = cnt
+        res = requests.get(REQ_URL, req_params)
+        res_code = res.status_code
+        res = json.loads(res.text)
+        if res_code != 200:
+            print(
+                f"""
+            ErrorCode -> {res_code}\n
+            Error -> {res['error']}\n
+            Page -> {cnt}"""
+            )
+        else:
+            if res["hits"] == 0:
+                print("返ってきた商品数の数が0なので、ループ終了")
+                break
+            tmp_df = pd.DataFrame(res["Items"])[WANT_ITEMS]
+            df = pd.concat([df, tmp_df], ignore_index=True)
+        if cnt == MAX_PAGE:
+            print("MAX PAGEに到達したので、ループ終了")
+            break
+        # logger.info(f"{cnt} end!")
+        cnt += 1
+        # リクエスト制限回避
+        sleep(1)
+        print("Finished!!")
+
+    df["platform_id"] = platform_id
+    df["knowledge_id"] = knowledge_id
+    df["size_id"] = size_id
+    df_main = df.rename(
+        columns={"itemName": "name", "itemPrice": "price", "itemUrl": "url"}  # noqa
+    )
+    df_main = df_main.reindex(
+        columns=[
+            "id",
+            "name",
+            "url",
+            "price",
+            "knowledge_id",
+            "platform_id",
+            "size_id",
+        ]
+    )
+    print("price type before:", df_main["price"].dtype)
+    df_main["price"] = df_main["price"].astype(int)
+    print("price type after:", df_main["price"].dtype)
+    run_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    df_main["created_at"] = run_time
+    df_main["updated_at"] = run_time
+    return df_main
