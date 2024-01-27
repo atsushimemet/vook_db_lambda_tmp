d8b612040630230ba74709917775cd69ac0aefae
atsushimemet <noap3b69n@gmail.com>
2024-01-27 21:00:13 +0900

    Feature run local (#1)
    * add: query_ng_ok.csv
    
    * update: gitignore
    
    * update: req pyarrow
    
    * update: success get_knowledges
    
    * refactor: chg var name ng_ok_table
    
    * fix: create_word_list内での_nameカラムの更新修正
    
    * update: success df_no_ng_keyword
    
    * add: time_decorator
    
    * update: success df_bulk
    
    * add: todo revise id
    
    * add: todo upload s3
    
    * update: repeating is too long
    
    * update: success run if check
    
    * update: success upload_s3
    
    * update: upload s3 func identifying env
    
    * add: read_csv_from_s3
    
    * update: success df_prev
    
    * update: id revise for initial running
    
    * update: success put products
    
    * update: put_products counter
    
    * update: success all
    
    * fix: how to create s3 client
    
    * update: arg profile_name
    
    * add: zip ver 1
    
    * add: zip ver 2

--- rakuten_api_call_bulk_from_table.py
+++ rakuten_api_call_bulk_from_table.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import numpy as np
-import pandas as pd
 
+from vook_db_v7.local_config import s3_bucket, s3_file_name_products_raw_prev
 from vook_db_v7.rds_handler import get_knowledges, get_products, put_products
 from vook_db_v7.tests import run_all_if_checker
 from vook_db_v7.utils import (
     create_df_no_ng_keyword,
     create_wort_list,
+    read_csv_from_s3,
     repeat_dataframe_maker,
     upload_s3,
 )
@@ -27,15 +27,31 @@ def main(event, context):
     )
     # df_bulkの作成
     df_bulk = repeat_dataframe_maker(df_no_ng_keyword)
-    df_prev = pd.read_csv("./data/output/products_raw_prev.csv")
-    PREV_ID_MAX = df_prev["id"].max()
-    df_bulk["id"] = np.arange(PREV_ID_MAX, PREV_ID_MAX + len(df_bulk)) + 1
+
+    # IDの設定
+    df_prev = read_csv_from_s3(s3_bucket, s3_file_name_products_raw_prev)
+    nan_arr = np.isnan(df_prev["id"])
+    if all(nan_arr):
+        df_bulk["id"] = np.arange(1, len(df_bulk) + 1)
+    elif any(nan_arr):
+        Exception("一部に欠損が生じているという想定外の事象です。")
+    else:
+        PREV_ID_MAX = df_prev["id"].max()
+        df_bulk["id"] = np.arange(PREV_ID_MAX, PREV_ID_MAX + len(df_bulk)) + 1
+
     run_all_if_checker(df_bulk)
     # df_bulkをs３に保存
     df = df_bulk
     upload_s3(df)
+
     # df_bulkをRDSに保存
     put_products(df_bulk)
     # RDSに保存したデータを確認
     df_from_db = get_products()
-    print(df_from_db.head(), df_from_db.shape)
+    print("shape:", df_from_db.shape)
+    print("id min:", df_from_db["id"].min())
+    print("id max:", df_from_db["id"].max())
+
+
+if __name__ == "__main__":
+    main(1, 1)
--- vook_db_v7/utils.py
+++ vook_db_v7/utils.py
@@ -2,8 +2,10 @@ import base64
 import datetime
 import hashlib
 import json
+import os
 import re
-from io import StringIO
+import time
+from io import BytesIO, StringIO
 from time import sleep
 
 import boto3
@@ -20,7 +22,7 @@ from vook_db_v7.config import (
     size_id,
     sleep_second,
 )
-from vook_db_v7.local_config import s3_bucket, s3_key
+from vook_db_v7.local_config import s3_bucket, s3_file_name_products_raw_prev
 
 
 def DataFrame_maker(keyword, platform_id, knowledge_id, size_id):
@@ -83,51 +85,48 @@ def DataFrame_maker(keyword, platform_id, knowledge_id, size_id):
 
 
 # エラーワードに対して対応表をもとにレスポンスする関数
-def convertor(input_string, errata_table):
+def convertor(input_string, ng_ok_table):
     # 特定のワードが DataFrame に含まれているかどうかを確認し、行番号を表示
-    row_indices = errata_table.index[
-        errata_table.apply(lambda row: input_string in row.values, axis=1)
+    row_indices = ng_ok_table.index[
+        ng_ok_table.apply(lambda row: input_string in row.values, axis=1)
     ].tolist()
     if row_indices:
-        output = errata_table["corrected"][row_indices[0]]
+        output = ng_ok_table["corrected"][row_indices[0]]
         print(f"{input_string}を{output}に変換します")
         return output
-
     else:
         print(f"{input_string}は対応表に存在しません。")
         return input_string
 
 
 # 対応表を読み出し
-errata_table = pd.read_csv("./data/input/query_ng_ok.csv")
+ng_ok_table = pd.read_csv("./data/input/query_ng_ok.csv")
 
 
 def validate_input(input_string):
     """
     連続する2文字以上で構成されたワードのみをOKとし、単体1文字またはスペースの前後に単体1文字が含まれるワードをNGとするバリデータ関数
     """
-    print("対応表", errata_table)
     # 正規表現パターン: 単体1文字またはスペースの前後に単体1文字が含まれるワードを検出
     pattern_ng = re.compile(r"^[!-~]$|\s[!-~]$|^[!-~]\s")
-
-    # 入力文字列がOKパターンに一致するか確認
     # 入力文字列がNGパターンに一致するか確認
     if not pattern_ng.search(input_string):
         return input_string
-
     else:
         # エラーワードがあればメッセージを吐き、convertor関数によって対応する
         print(f"エラーワード　{input_string}が存在しました:")
-        return convertor(input_string, errata_table)
+        return convertor(input_string, ng_ok_table)
 
 
-def create_wort_list(df_from_db, unit):
-    # 対象のワードリスト作成
-    words = df_from_db[f"{unit}_name"].values
+def create_wort_list(df_from_db: pd.DataFrame, unit: str) -> list:
+    """brand,line,knowledgeの連続2文字以上ワードかどうかを判定、修正する"""
+    words = df_from_db[
+        f"{unit}_name"
+    ].values.copy()  # NOTE:copyしないと関数内部で_nameカラムが更新される。
     for row in np.arange(len(words)):
         word = words[row]
         words[row] = validate_input(word)
-    return words
+    return list(words)
 
 
 def create_df_no_ng_keyword(
@@ -141,6 +140,18 @@ def create_df_no_ng_keyword(
     return df_no_ng_keyword
 
 
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__}の実行時間: {round(end_time - start_time)}秒")
+        return result
+
+    return wrapper
+
+
+@time_decorator
 def repeat_dataframe_maker(
     df_no_ng_keyword,
     platform_id=platform_id,
@@ -149,26 +160,35 @@ def repeat_dataframe_maker(
 ):
     n_bulk = len(df_no_ng_keyword)
     df_bulk = pd.DataFrame()
-    for n in np.arange(n_bulk):
+    for i, n in enumerate(np.arange(n_bulk)):
         brand_name = df_no_ng_keyword.brand_name[n]
         line_name = df_no_ng_keyword.line_name[n]
         knowledge_name = df_no_ng_keyword.knowledge_name[n]
         query = f"{brand_name} {line_name} {knowledge_name} 中古"
         # query validatorが欲しい　半角1文字をなくす
-
         knowledge_id = df_no_ng_keyword.knowledge_id[n]
-        print("検索キーワード:[" + query + "]", "knowledge_id:", knowledge_id)
+        print(f"{i:03}, 検索キーワード:[" + query + "]", "knowledge_id:", knowledge_id)
         output = DataFrame_maker(query, platform_id, knowledge_id, size_id)
         df_bulk = pd.concat([df_bulk, output], ignore_index=True)
         sleep(sleep_second)
         break
         # 429エラー防止のためのタイムストップ
-    return df_bulk
+    return df_bulk  # TODO:lambda実行でempty dataframe 原因調査から
 
 
-def upload_s3(df, s3_bucket=s3_bucket, s3_key=s3_key):
-    # S3にアップロードするためのBoto3クライアントを作成
-    s3_client = boto3.client("s3")
+def upload_s3(
+    df, s3_bucket=s3_bucket, s3_key=s3_file_name_products_raw_prev, profile_name="vook"
+):
+    # Lambda環境を識別するための環境変数の存在をチェック
+    if "AWS_LAMBDA_FUNCTION_NAME" in os.environ:
+        # Lambda環境用のクライアント初期化
+        print("Now: Lambda env")
+        s3_client = boto3.client("s3")
+    else:
+        # ローカル開発環境用のクライアント初期化
+        print("Now: Local env")
+        session = boto3.session.Session(profile_name=profile_name)
+        s3_client = session.client("s3")
     # Pandas DataFrameをCSV形式の文字列に変換
     csv_data = df.to_csv(index=False)
     # 文字列IOを使ってCSVデータを書き込む
@@ -184,6 +204,29 @@ def upload_s3(df, s3_bucket=s3_bucket, s3_key=s3_key):
     content_md5 = base64.b64encode(file_hash).decode("utf-8")
     # S3にCSVファイルをアップロード
     s3_client.put_object(
-        Body=csv_binary, Bucket=s3_bucket, Key=s3_key, ContentMD5=content_md5
+        Body=csv_binary,
+        Bucket=s3_bucket,
+        Key=s3_file_name_products_raw_prev,
+        ContentMD5=content_md5,
     )
-    print(f"CSV file uploaded to s3://{s3_bucket}/{s3_key}")
+    print(f"CSV file uploaded to s3://{s3_bucket}/{s3_file_name_products_raw_prev}")
+
+
+def read_csv_from_s3(bucket_name, file_key, profile_name="vook"):
+    # Lambda環境を識別するための環境変数の存在をチェック
+    if "AWS_LAMBDA_FUNCTION_NAME" in os.environ:
+        # Lambda環境用のクライアント初期化
+        print("Now: Lambda env")
+        s3_client = boto3.client("s3")
+    else:
+        # ローカル開発環境用のクライアント初期化
+        print("Now: Local env")
+        session = boto3.session.Session(profile_name=profile_name)
+        s3_client = session.client("s3")
+    # S3バケットからファイルを読み込む
+    response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+    # レスポンスからバイナリデータを取得
+    file_content = response["Body"].read()
+    # バイナリデータをPandas DataFrameに変換
+    df = pd.read_csv(BytesIO(file_content), encoding="utf-8")
+    return df
