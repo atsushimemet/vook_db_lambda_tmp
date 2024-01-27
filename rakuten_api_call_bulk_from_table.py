ec8ef3c3af12e55eba4d12cc7ff831d89a40c119
atsushimemet <noap3b69n@gmail.com>
2024-01-27 12:41:24 +0900

    add: todo revise id

--- rakuten_api_call_bulk_from_table.py
+++ rakuten_api_call_bulk_from_table.py
@@ -30,6 +30,13 @@ def main(event, context):
     # df_prev = pd.read_csv("./data/output/products_raw_prev.csv")
     # PREV_ID_MAX = df_prev["id"].max()
     # df_bulk["id"] = np.arange(PREV_ID_MAX, PREV_ID_MAX + len(df_bulk)) + 1
+
+    # TODO:IDの更新
+    # df_from_db_prev = get_products()
+    # PREV_ID_MAX = df_from_db_prev["id"].max()
+    # df_bulk["id"] = np.arange(PREV_ID_MAX, PREV_ID_MAX + len(df_bulk)) + 1
+    # assert df_bulk["id"].min() == df_from_db_prev["id"].max() + 1
+
     # run_all_if_checker(df_bulk)
     # # df_bulkをs３に保存
     # df = df_bulk
