6c625c0385f93cdea3669e4e82dff73be90a2c53
atsushimemet <noap3b69n@gmail.com>
2024-02-02 09:05:39 +0900

    update: apply set_id to yahoo

--- yahoo_api_call_bulk_from_table.py
+++ yahoo_api_call_bulk_from_table.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import numpy as np
-
-from vook_db_v7.config import platform_id, s3_bucket, s3_file_name_products_raw_prev
+from vook_db_v7.config import platform_id
 from vook_db_v7.rds_handler import get_products, put_products
 from vook_db_v7.tests import run_all_if_checker
 from vook_db_v7.utils import (
     DataFrame_maker_yahoo,
     create_api_input,
-    read_csv_from_s3,
     repeat_dataframe_maker,
+    set_id,
     upload_s3,
 )
 
@@ -20,16 +18,7 @@ def main(event, context):
     df_api_input = create_api_input()
     # df_bulkの作成
     df_bulk = repeat_dataframe_maker(df_api_input, platform_id, DataFrame_maker_yahoo)
-    # IDの設定
-    df_prev = read_csv_from_s3(s3_bucket, s3_file_name_products_raw_prev)
-    nan_arr = np.isnan(df_prev["id"])
-    if all(nan_arr):
-        df_bulk["id"] = np.arange(1, len(df_bulk) + 1)
-    elif any(nan_arr):
-        Exception("一部に欠損が生じているという想定外の事象です。")
-    else:
-        PREV_ID_MAX = df_prev["id"].max()
-        df_bulk["id"] = np.arange(PREV_ID_MAX, PREV_ID_MAX + len(df_bulk)) + 1
+    df_bulk = set_id(df_bulk)
     run_all_if_checker(df_bulk)
     # df_bulkをs３に保存
     upload_s3(df_bulk)
