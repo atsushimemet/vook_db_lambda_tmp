c533bd877dd000680327dc7d7598840bef05e646
atsushimemet <noap3b69n@gmail.com>
2024-02-12 11:26:56 +0900

    refactor: adequate var name

--- tests/test_get_knowledges.py
+++ tests/test_get_knowledges.py
@@ -5,7 +5,7 @@ from vook_db_v7.local_config import SSH_PKEY_PATH, get_ec2_config
 from vook_db_v7.rds_handler import get_knowledges
 
 
-class TestGetKnowledgesWrong:
+class TestGetKnowledgesInvalid:
     def test_not_get_knowledges_wrong_end_point(self):
         wrong_config = {
             "host_name": "3.114.154.242",
@@ -29,7 +29,7 @@ def df_knowledge_brand_line(request):
 
 
 @pytest.mark.usefixtures("df_knowledge_brand_line")
-class TestGetKnowledgesRight:
+class TestGetKnowledgesValid:
     def test_columns_name(self):
         actual = self.df.columns.tolist()
         expected = ["knowledge_id", "knowledge_name", "brand_name", "line_name"]
