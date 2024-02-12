807f370e535bf4e570b3a3a1ab8db939eece862f
atsushimemet <noap3b69n@gmail.com>
2024-02-12 11:25:30 +0900

    refactor: Wrong for Right

--- tests/test_get_knowledges.py
+++ tests/test_get_knowledges.py
@@ -5,19 +5,20 @@ from vook_db_v7.local_config import SSH_PKEY_PATH, get_ec2_config
 from vook_db_v7.rds_handler import get_knowledges
 
 
-def test_not_get_knowledges_wrong_end_point():
-    wrong_config = {
-        "host_name": "3.114.154.242",
-        "ec2_port": 22,
-        "ssh_username": "ec2-user",
-        "ssh_pkey": SSH_PKEY_PATH,
-        "rds_end_point": "wrong_end_point",
-        "rds_port": 3306,
-    }
+class TestGetKnowledgesWrong:
+    def test_not_get_knowledges_wrong_end_point(self):
+        wrong_config = {
+            "host_name": "3.114.154.242",
+            "ec2_port": 22,
+            "ssh_username": "ec2-user",
+            "ssh_pkey": SSH_PKEY_PATH,
+            "rds_end_point": "wrong_end_point",
+            "rds_port": 3306,
+        }
 
-    msg = "Lost connection to MySQL server during query"
-    with pytest.raises(pymysql.MySQLError, match=rf".*{msg}.*"):
-        get_knowledges(wrong_config)
+        msg = "Lost connection to MySQL server during query"
+        with pytest.raises(pymysql.MySQLError, match=rf".*{msg}.*"):
+            get_knowledges(wrong_config)
 
 
 @pytest.fixture(scope="class")
